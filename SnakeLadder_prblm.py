991fa23c9153b0c31eca44e1ff20ad25707f7ba4
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-03-10 15:26:19 +0530

    Add some condition in snake ladder problem

--- SnakeLadder_prblm.py
+++ SnakeLadder_prblm.py
@@ -6,3 +6,13 @@ print("Single player at start position",pos)
 def roll_dice():
     dice=random.randint(1,6)
     return dice
+def choose_option(dice):
+    option=random.randint(1,3)
+    if(option==1):
+        print("No move")
+    elif(option==2):
+        pos+=dice
+    else:
+        pos-=dice
+        
+
