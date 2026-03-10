c4e2daa919a91d114897b29b3dc930bf120ac3de
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-03-10 15:41:32 +0530

    Added Repeat till the player reaches the winning position 100

--- SnakeLadder_prblm.py
+++ SnakeLadder_prblm.py
@@ -7,12 +7,22 @@ def roll_dice():
     dice=random.randint(1,6)
     return dice
 def choose_option(dice):
+    global pos
     option=random.randint(1,3)
-    if(option==1):
-        print("No move")
-    elif(option==2):
+    if(option==2):
         pos+=dice
-    else:
+    elif(option==3):
         pos-=dice
+        if(pos<0):
+            pos=0
+    return pos
+
+while(pos<100):
+    dice=roll_dice()
+    pos=choose_option(dice)
+
+if(pos>=100):
+    print("Player1 win")
+   
         
 
